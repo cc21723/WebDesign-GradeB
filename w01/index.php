169b786e956e9ec54ac5cbfc62900d3d52f78ae3
snow <83638396+cc21723@users.noreply.github.com>
2025-07-01 10:50:45 +0800

    完成顯示五筆最新消息在首頁

--- w01/index.php
+++ w01/index.php
@@ -34,8 +34,17 @@
 				</div>
 			</div>
 			<div class="di" style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
-				<marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
+				
+			<marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
+					<?php
+					 $ads=$Ad->all(['sh' => 1]);
+					 foreach($ads as $ad){
+						echo $ad['text'];
+						echo "&nbsp;&nbsp;&nbsp;&nbsp;";
+					 } 
+					 ?>
 				</marquee>
+				
 				<div style="height:32px; display:block;"></div>
 				<!--正中央-->
 
@@ -75,6 +84,17 @@
 					<span class="t botli">最新消息區
 					</span>
 					<ul class="ssaa" style="list-style-type:decimal;">
+						<?php
+							$new = $News->all(['sh'=>1]," limit 5");
+							foreach($new as $n){
+								echo "<li>";
+								echo mb_substr($n['text'],0,25);
+								echo "<span class='all' style='display:none'>";
+								echo $n['text']; 
+								echo "</span>";
+								echo "</li>";
+							}
+						?>
 					</ul>
 					<div id="altt" style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
 					<script>
