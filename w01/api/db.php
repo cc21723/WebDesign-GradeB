cd3af97a3f19375cf267341ffb4a83c8d0e12399
snow <83638396+cc21723@users.noreply.github.com>
2025-07-01 11:06:59 +0800

    進行more判斷並在db.php中增count函式

--- w01/api/db.php
+++ w01/api/db.php
@@ -6,100 +6,126 @@ session_start();
 date_default_timezone_set("Asia/Taipei");
 
 //查詢
-function q($sql){
+function q($sql)
+{
     $dsn = "mysql:host=localhost;dbname=db15;charset=utf8";
-    $pdo = new PDO ($dsn,'root','');
+    $pdo = new PDO($dsn, 'root', '');
     return $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
 }
 
 // 輸出
-function dd($array){
+function dd($array)
+{
     echo "<pre>";
     print_r($array);
     echo "</pre>";
 }
 
 //頁面跳轉
-function to($url){
-    header("location:".$url);
+function to($url)
+{
+    header("location:" . $url);
 }
 
-class DB{
+class DB
+{
     private $dsn = "mysql:host=localhost;dbname=db15;charset=utf8";
     private $pdo;
     private $table;
 
-    function __construct($table){
+    function __construct($table)
+    {
         $this->table = $table;
-        $this->pdo = new PDO($this->dsn,'root','');        
+        $this->pdo = new PDO($this->dsn, 'root', '');
     }
 
-    function all(...$arg){
+    function all(...$arg)
+    {
         $sql = "select * from $this->table";
-        if(isset($arg[0])){
-            if(is_array($arg[0])){
+        if (isset($arg[0])) {
+            if (is_array($arg[0])) {
                 $tmp = $this->arraytosql($arg[0]);
-                $sql=$sql." where ".join(" AND ", $tmp);
-
-            }else{
+                $sql = $sql . " where " . join(" AND ", $tmp);
+            } else {
                 $sql .= $arg[0];
             }
         }
-        if(isset($arg[1])){
+        if (isset($arg[1])) {
             $sql .= $arg[1];
         }
         // echo $sql;
         return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
     }
 
-    function find($id){
-        $sql = "select * from $this->table ";
-        
-            if(is_array($id)){
-                $tmp=$this->arraytosql($id);
-                $sql=$sql." WHERE ".join(" AND ", $tmp);
-
-            }else{
-                $sql .= " WHERE `id`='$id'";
+    function count(...$arg)
+    {
+        $sql = "select count(*) from $this->table";
+        if (isset($arg[0])) {
+            if (is_array($arg[0])) {
+                $tmp = $this->arraytosql($arg[0]);
+                $sql = $sql . " where " . join(" AND ", $tmp);
+            } else {
+                $sql .= $arg[0];
             }
+        }
+        if (isset($arg[1])) {
+            $sql .= $arg[1];
+        }
+        // echo $sql;
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+
+
+    function find($id)
+    {
+        $sql = "select * from $this->table ";
+
+        if (is_array($id)) {
+            $tmp = $this->arraytosql($id);
+            $sql = $sql . " WHERE " . join(" AND ", $tmp);
+        } else {
+            $sql .= " WHERE `id`='$id'";
+        }
         // echo $sql;
         return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
 
-    function save($array){
-        if(isset($array['id'])){
+    function save($array)
+    {
+        if (isset($array['id'])) {
             //update
             $sql = "update $this->table set ";
             $tmp = $this->arraytosql($array);
-            $sql.= join(" , ",$tmp) . "where `id`= '{$array['id']}'";
-        }else{
+            $sql .= join(" , ", $tmp) . "where `id`= '{$array['id']}'";
+        } else {
             //insert
-            $cols = join("`,`",array_keys($array));
-            $values = join("','",$array);
+            $cols = join("`,`", array_keys($array));
+            $values = join("','", $array);
             $sql = "insert into $this->table(`$cols`) values('$values')";
         }
         // echo $sql;
         return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
     }
 
-    function del($id){
-         $sql = "delete from $this->table";
-        
-            if(is_array($id)){
-                $tmp = $this->arraytosql($id);
-                $sql=$sql." where ".join(" AND ", $tmp);
+    function del($id)
+    {
+        $sql = "delete from $this->table";
 
-            }else{
-                $sql .= " WHERE `id`='$id'";
-            }
+        if (is_array($id)) {
+            $tmp = $this->arraytosql($id);
+            $sql = $sql . " where " . join(" AND ", $tmp);
+        } else {
+            $sql .= " WHERE `id`='$id'";
+        }
         // echo $sql;
         return $this->pdo->exec($sql);
     }
 
-    private function arraytosql($array){
-        $tmp=[];
-        foreach($array as $key => $vaule){
-            $tmp[]="`$key`='$vaule'";
+    private function arraytosql($array)
+    {
+        $tmp = [];
+        foreach ($array as $key => $vaule) {
+            $tmp[] = "`$key`='$vaule'";
         }
         return $tmp;
     }
@@ -115,12 +141,3 @@ $Bottom = new DB('bottom'); //頁尾版權資料管理
 $News = new DB('news');     //最新消息資料管理
 $Admin = new DB('admin');       //管理者帳號管理
 $Menu = new DB('menu');     //選單管理
-
-
-
-
-
-
-
-
-?>
