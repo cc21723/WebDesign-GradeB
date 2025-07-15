020dbb9e051c9755ac43893d817c6c93e2b4516b
snow <83638396+cc21723@users.noreply.github.com>
2025-07-15 15:32:40 +0800

    完成人氣文章彈出文章功能

--- w02/front/pop.php
+++ w02/front/pop.php
@@ -1,6 +1,26 @@
 <div class="nav" style="margin-bottom: 20px;">
     目前位置：首頁 > 人氣文章區 </span>
 </div>
+<style>
+    .title{
+        cursor: pointer;
+        color: blue;
+    }
+    .title:hover{
+        text-decoration: underline;
+        color:green;
+    }
+    .pop {
+			background: rgba(51, 51, 51, 0.8);
+			color: #FFF;
+            height: 400px;
+			width: 500px;
+			position: fixed;
+			display: none;
+			z-index: 9999;
+			overflow: auto;
+		}
+</style>
 <table style="width: 95%; margin:auto;">
     <tr class="ct">
         <td width="20%">標題</td>
@@ -18,10 +38,17 @@
     foreach ($rows as $idx => $row):
     ?>
         <tr>
-            <td><?=$row['title'];?></td>
+            <td class="title"><?= $row['title']; ?></td>
             <td>
-                <div class="short"><?=mb_substr($row['text'],0,30);?>...</div>
-                <div class="all"></div>
+                <div class="short"><?= mb_substr($row['text'], 0, 30); ?>...</div>
+                <div class="all">
+                    <div id="alerr" class="pop">
+                        <h2><?=$Type[$row['type']];?></h2>
+                        <pre id="ssaa">
+                            <?=$row['text'];?>
+                        </pre>
+                    </div>
+                </div>
             </td>
             <td></td>
         </tr>
@@ -29,3 +56,33 @@
     endforeach;
     ?>
 </table>
+
+<div class="ct">
+    <?php
+
+    if ($now - 1 > 0) {
+        echo "<a href='?do=news&p=" . ($now - 1) . "' style='font-size:18px'> < </a>";
+    }
+
+    for ($i = 1; $i <= $pages; $i++) {
+        $size = ($i == $now) ? '24px' : '20px';
+        echo "<a href='?do=news&p=$i' style='font-size:{$size}'> $i </a>";
+    }
+
+    if ($now + 1 <= $pages) {
+        echo "<a href='?do=news&p=" . ($now + 1) . "' style='font-size:18px'> > </a>";
+    }
+    ?>
+</div>
+
+<script>
+    $(".title").hover(function () {
+            //in
+            $(this).next().find(".pop").show();
+            
+        }, function () {
+            // out
+            $(this).next().find(".pop").hide();
+        }
+    );
+</script>
