652d798eaf1647a1197a6d35594198c40cc7c182
snow <83638396+cc21723@users.noreply.github.com>
2025-07-22 15:23:28 +0800

    debug

--- w03/api/add_poster.php
+++ w03/api/add_poster.php
@@ -2,11 +2,11 @@
 
 if(!empty($_FILES['img']['tmp_name'])){
     move_uploaded_file($_FILES['img']['tmp_name'],"../images/".$_FILES['img']['name']);
-    $Porster->save([
+    $Poster->save([
         'name'=>$_POST['name'],
         'img'=>$_FILES['img']['name'],
         'sh'=>1,
-        'rank'=>$Porster->max('id')+1,  //排序 現在存在的最大id值+1就是下一個
+        'rank'=>$Poster->max('id')+1,  //排序 現在存在的最大id值+1就是下一個
         'ani'=>rand(1,3)  //動畫一到三
     ]);
 }
