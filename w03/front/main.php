3797151c01d7a87e1e20f64cd7b6d36ed5d2f9e5
snow <83638396+cc21723@users.noreply.github.com>
2025-07-25 10:40:08 +0800

    顯示下方海報按鈕區

--- w03/front/main.php
+++ w03/front/main.php
@@ -1,110 +1,169 @@
-<style>
-    .lists {
-        width: 210px;
-        height: 240px;
-        margin: 0 auto;
-        position: relative;
-        overflow: hidden;
-    }
+    <style>
+        .lists {
+            width: 210px;
+            height: 240px;
+            margin: 0 auto;
+            position: relative;
+            overflow: hidden;
+        }
 
-    .btns {
-        width: 320px;
-        height: 120px;
+        .btns {
+            width: 320px;
+            height: 120px;
+            display: flex;
+            overflow: hidden;
+        }
 
-    }
+        .left,
+        .right {
+            width: 0;
+            height: 0;
+            border-top: 20px solid transparent;
+            border-bottom: 20px solid transparent;
 
-    .left,
-    .right {
-        width: 0;
-        height: 0;
-        border-top: 20px solid transparent;
-        border-bottom: 20px solid transparent;
+        }
 
-    }
+        .left {
+            border-left: 0px solid black;
+            border-right: 30px solid black;
 
-    .left {
-        border-left: 0px solid black;
-        border-right: 30px solid black;
+        }
 
-    }
+        .right {
+            border-left: 30px solid black;
+            border-right: 0px solid black;
 
-    .right {
-        border-left: 30px solid black;
-        border-right: 0px solid black;
+        }
 
-    }
+        .controls {
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+        }
 
-    .controls {
-        display: flex;
-        justify-content: space-around;
-        align-items: center;
-    }
+        .poster {
+            text-align: center;
+            position: absolute;
+            width: 210px;
+            height: 240px;
+            display: none;
+        }
 
-    .poster {
-        text-align: center;
-        position: absolute;
-        width: 210px;
-        height: 240px;
-        display: none;
-    }
+        .poster img {
+            width: 200px;
+            height: 220px;
 
-    .poster img {
-        width: 200px;
-        height: 220px;
-
-    }
-</style>
-<?php
-$posters = $Poster->all(['sh' => 1], " order by `rank` ");
-
-?>
-
-<div class="half" style="vertical-align:top;">
-    <h1>預告片介紹</h1>
-    <div class="rb tab" style="width:95%;">
-        <div id="abgne-block-20111227">
-            <div class="lists">
-                <?php
-                foreach ($posters as $poster):
-                ?>
-                    <div class="poster">
-                        <img src="./images/<?= $poster['img']; ?>">
-                        <div><?= $poster['name']; ?></div>
-                    </div>
-                <?php
-                endforeach;
-                ?>
-            </div>
-            <div class="controls">
-                <div class="left"></div>
-                <div class="btns">
-
+        }
+        .poster-btn{
+            width: 80px;
+            height: 100px;
+            display: inline-block;
+            flex-shrink: 0;
+        }
+        .poster-btn img{
+            width: 70px;
+            height: 90px;
+        }
+    </style>
+    <?php
+    $posters = $Poster->all(['sh' => 1], " order by `rank`");
+    ?>
+    <div class="half" style="vertical-align:top;">
+        <h1>預告片介紹</h1>
+        <div class="rb tab" style="width:95%;">
+            <div>
+                <div class="lists">
+                    <?php
+                    foreach ($posters as $poster):
+                    ?>
+                        <div class="poster" data-ani='<?= $poster['ani']; ?>' data-id='<?= $poster['id']; ?>'>
+                            <img src="./images/<?= $poster['img']; ?>">
+                            <div><?= $poster['name']; ?></div>
+                        </div>
+                    <?php
+                    endforeach;
+                    ?>
                 </div>
-                <div class="right"></div>
+
+                <div class="controls">
+                    <div class="left"></div>
+                    <div class="btns">
+                        <?php
+                        foreach ($posters as $poster):
+                        ?>
+                            <div class="poster-btn" data-ani='<?= $poster['ani']; ?>' data-id="<?= $poster['id']; ?>">
+                                <img src="./images/<?= $poster['img']; ?>">
+                                <div><?= $poster['name']; ?></div>
+                            </div>
+                        <?php
+                        endforeach; ?>
+                    </div>
+                    <div class="right"></div>
+                </div>
             </div>
         </div>
     </div>
-</div>
-<script>
-    let rank = 0;
-    $(".poster").eq(rank).show();
-    let slider = setInterval(()=>{
-        rank++;
-        if(rank>$(".poster").length-1){
-            rank = 0;
-        }
-        $(".poster").hide();
+    <script>
+        let rank = 0;
         $(".poster").eq(rank).show();
-    },2000)
-</script>
-<div class="half">
-    <h1>院線片清單</h1>
-    <div class="rb tab" style="width:95%;">
-        <table>
-            <tbody>
-                <tr> </tr>
-            </tbody>
-        </table>
-        <div class="ct"> </div>
+        let slider = setInterval(() => {
+
+            //只能輪播
+            // rank++;
+            // if(rank>$(".poster").length-1){
+            //     rank = 0;
+            // }
+            // $(".poster").hide();
+            // $(".poster").eq(rank).show();
+
+            animater();
+
+        }, 2000)
+
+        function animater() {
+            let now = $(".poster:visible");
+            rank++;
+            if (rank > $(".poster").length - 1) {
+                rank = 0;
+            }
+
+            let next = $(".poster").eq(rank);
+            let ani = $(now).data('ani');
+            console.log(ani);
+            switch (ani) {
+                case 1:
+                    //淡入淡出
+                    $(now).fadeOut(1000);
+                    $(next).fadeIn(1000);
+                    break;
+                case 2:
+                    //縮放
+                    $(now).hide(1000);
+                    $(next).show(1000);
+                    break;
+                case 3:
+                    //滑入滑出
+                    $(now).slideUp(1000);
+                    $(next).slideDown(1000);
+                    break;
+            }
+
+        }
+    </script>
+
+
+    <div class="half">
+        <h1>院線片清單</h1>
+        <div class="rb tab" style="width:95%;">
+            <table>
+                <tbody>
+                    <tr>
+
+                    </tr>
+                </tbody>
+            </table>
+            <div class="ct">
+
+            </div>
+        </div>
     </div>
-</div>
