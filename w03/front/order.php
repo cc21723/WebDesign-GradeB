0a2b567b2f842e58ac63262f0d0b52bf43dcc017
snow <83638396+cc21723@users.noreply.github.com>
2025-07-28 16:13:52 +0800

    增加日期選單變動事件

--- w03/front/order.php
+++ w03/front/order.php
@@ -1,47 +1,38 @@
+<h3 class='ct'>線上訂票</h3>
+
 <style>
-    table{
-        width: 60%;
-        margin: 10px auto;
-        padding: 20px;
-        border: 1px solid #ccc;
-        background-color: #aaa;
-    }
-    table td{
-        background-color: #ccc;
-    }
-    table td:nth-child(1){
-        width: 20%;
-        text-align: right;
-    }
-    table td:nth-child(2) select{
-        width: 98%;
-    }
+#orderForm{
+    width:50%;
+    margin:10px auto;
+    padding:20px;
+    border:1px solid #ccc;
+    background:#aaa;
+}    
+#orderForm td{
+    background:#ccc;
+}
+#orderForm td:nth-child(1){
+    width:20%;
+    text-align:right;
+}
+#orderForm td:nth-child(2) select{
+    width:98%;
+    text-align:left;
+}
 </style>
-<h3 class="ct">線上訂票</h3>
-<table>
+
+<table id='orderForm'>
     <tr>
         <td>電影：</td>
-        <td>
-            <select name="movie" id="movie">
-
-            </select>
-        </td>
+        <td><select name="movie" id="movie"></select></td>
     </tr>
     <tr>
-        <td style="background: dimgrey;">日期：</td>
-        <td>
-            <select name="date" id="date">
-
-            </select>
-        </td>
+        <td>日期：</td>
+        <td><select name="date" id="date"></select></td>
     </tr>
     <tr>
         <td>場次：</td>
-        <td>
-            <select name="session" id="session">
-
-            </select>
-        </td>
+        <td><select name="session" id="session"></select></td>
     </tr>
 </table>
 <div class="ct">
@@ -50,20 +41,44 @@
 </div>
 
 <script>
-    let url = new URLSearchParams(location.search);
-    
-    getMovies();
+let url=new URLSearchParams(location.search);
 
-    function getMovies() { 
-        let id = 0;
-        
-        if(url.has('id')){
-            id = url.get('id');
-        }
+getMovies();
 
-        $.get("./api/get_movie.php",{id},(movies)=>{
-            $("#movie").html(movies);
-        })
-     }
+$("#movie").on("change",function(){
+    getDates($(this).val())
+})
+$("#date").on("change",function(){
+    getSessions($("#movie").val(),$(this).val())
+})
 
+function getMovies(){
+    let id=0
+if(url.has('id')){
+    id=url.get('id');
+}
+
+$.get("./api/get_movie.php",{id},(movies)=>{
+    $("#movie").html(movies)
+    getDates($("#movie").val());
+})
+
+
+}
+
+
+function getDates(movieId){
+
+    $.get("./api/get_date.php",{movieId},(dates)=>{
+        $("#date").html(dates)
+        getSessions(movieId,$("#date").val())
+    })
+
+}
+
+function getSessions(movieId , date){
+    $.get("./api/get_session.php",{movieId,date},(sessions)=>{
+        $("#session").html(sessions)
+    })
+}
 </script>
