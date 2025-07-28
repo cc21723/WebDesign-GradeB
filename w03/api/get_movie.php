ab5a0ae6b7f6c2f73993d2047f5d55461580f900
snow <83638396+cc21723@users.noreply.github.com>
2025-07-28 14:21:35 +0800

    錯字

--- w03/api/get_movie.php
+++ w03/api/get_movie.php
@@ -7,7 +7,7 @@ $today = date("Y-m-d");
 $ondate = date("Y-m-d", strtotime("-2 days", strtotime($today)));
 $movies = $Movie->all(['sh' => 1], " and ondate between '$ondate' and '$today' order by `rank`");
 
-foreach($movise as $movie){
+foreach($movies as $movie){
     $selected = ($id==$movie['id'])?'selected':'';
     echo "<option value='{$movie['id']}' $selected>";
     echo $movie['name'];
