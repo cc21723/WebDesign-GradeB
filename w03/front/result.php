4a108aa2bef604f7edb4f834c6dcc78d99a40c02
snow <83638396+cc21723@users.noreply.github.com>
2025-08-01 14:32:36 +0800

    完成訂購單畫面 (美化非必要)

--- w03/front/result.php
+++ w03/front/result.php
@@ -1,1 +1,64 @@
-訂單
+<?php
+$order = $Order->find(['no' => $_GET['no']])
+?>
+<style>
+    .result-table {
+        width: 540px;
+        margin: 10px auto;
+        padding: 20px;
+        border: 1px solid #ccc;
+        background: #eee;
+    }
+
+    .result-table td {
+        background: #ccc;
+    }
+
+    .result-table td:nth-child(1) {
+        width: 20%;
+    } 
+    
+
+</style>
+<!-- 訂單 -->
+<table class="result-table">
+    <tr>
+        <td colspan="2">感謝您的訂購，您的訂單編號是：<?= $_GET['no']; ?></td>
+        <!-- <td></td> -->
+    </tr>
+    <tr>
+        <td>電影名稱：</td>
+        <td><?= $order['movie']; ?></td>
+    </tr>
+    <tr>
+        <td>日期：</td>
+        <td><?= $order['date']; ?></td>
+    </tr>
+    <tr>
+        <td>場次時間：
+        </td>
+        <td><?= $order['session']; ?></td>
+    </tr>
+    <tr>
+        <td colspan="2">
+            <div>座位：</div>
+            <?php
+            $seats = unserialize($order['seats']);
+            sort($seats);
+            foreach ($seats as $seat) {
+                echo "<div>";
+                echo floor($seat / 5) + 1 . "排" . ($seat % 5) + 1 . "號";
+                echo "</div>";
+            }
+            ?>
+            <div>共<?= $order['tickets']; ?>張電影票</div>
+        </td>
+        <!-- <td></td> -->
+    </tr>
+    <tr>
+        <td colspan="2">
+            <button onclick="location.href='index.php'">確定</button>
+        </td>
+        <!-- <td></td> -->
+    </tr>
+</table>
