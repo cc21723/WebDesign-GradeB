1a02685221a99d4daf582283d80b90c72fb3ef0d
snow <83638396+cc21723@users.noreply.github.com>
2025-08-15 13:39:07 +0800

    建立管理員管理的頁面

--- w04/back/admin.php
+++ w04/back/admin.php
@@ -1,1 +1,14 @@
-管理權限設置
+<!-- 管理權限設置 -->
+<div class="ct"><button onclick="location.href='?do=add_admin'">新增管理員</button></div>
+<table class="all">
+    <tr class="tt ct">
+        <td>帳號</td>
+        <td>密碼</td>
+        <td>管理</td>
+    </tr>
+    <tr class="pp ct">
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
