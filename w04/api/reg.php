2994bd0313c353031e75573aa5fcf883ecd486db
snow <83638396+cc21723@users.noreply.github.com>
2025-08-18 13:59:39 +0800

    完成編輯會員功能

--- w04/api/reg.php
+++ w04/api/reg.php
@@ -1,7 +1,10 @@
 <!-- 註冊api -->
+ <!-- 修改會員 -->
 <?php
 include_once "db.php";
 
-$_POST['regdate'] = date("Y-m-d");
-dd($_POST);
+if(!isset($_POST['id'])){
+    $_POST['regdate'] = date("Y-m-d");
+}
+// dd($_POST);
 $User->save($_POST);
--- w04/back/mem.php
+++ w04/back/mem.php
@@ -1,1 +1,44 @@
-會員管理
+<h2 class="ct">會員管理</h2>
+<table class="all">
+    <tr class="tt ct">
+        <td>姓名</td>
+        <td>會員帳號</td>
+        <td>註冊日期</td>
+        <td>操作</td>
+    </tr>
+
+    <?php
+    $mems=$User->all();
+    foreach ($mems as $mem):
+    ?>
+    
+    <tr class="pp ct">
+        <td><?=$mem['name'];?></td>
+        <td><?=$mem['acc'];?></td>
+        <td><?=$mem['regdate'];?></td>
+        <td>
+            <button class="edit-btn" data-id="<?= $mem['id']; ?>">修改</button>
+            <button class="del-btn" data-id="<?= $mem['id']; ?>">刪除</button>
+        </td>
+    </tr>
+    <?php
+    endforeach;
+    ?>
+</table>
+
+<script>
+     $(".edit-btn").on("click", function() {
+        let id = $(this).data("id");
+           location.href = "?do=edit_user&id="+id;
+    })
+
+    $(".del-btn").on("click", function() {
+        let id = $(this).data("id");
+        $.post("./api/del.php", {
+            id,
+            table: 'User'
+        }, () => {
+            location.reload();
+        })
+    })
+</script>
