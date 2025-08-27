3eb22f27fd20f619b6b5365d14f590b2744b0a09
snow <83638396+cc21723@users.noreply.github.com>
2025-08-27 10:28:35 +0800

    建立購物車session

--- w04/front/buycart.php
+++ w04/front/buycart.php
@@ -1,7 +1,21 @@
-<!-- 購物車 -->
+<!-- 建立購物車頁面的登入判斷 -->
 <?php
 if(!isset($_SESSION['login'])){
     to("?do=login");
 }
+
+?>
+
+<!-- 建立購物車session -->
+ <h2 class="ct"><?=$_SESSION['login'];?>的購物車</h2>
+
+<?php
+if(isset($_SESSION['cart']) && count($_SESSION['cart'])>0){
+    dd($_SESSION['cart']);
+}else{
+    
+    echo "購物車是空的";
+}    
+    
 
 ?>
