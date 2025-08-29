55015cf77eddcc1876d4f4e370a4227cd950dde5
snow <83638396+cc21723@users.noreply.github.com>
2025-08-29 11:03:44 +0800

    多講考試沒用的東西

--- w04/front/checkout.php
+++ w04/front/checkout.php
@@ -55,14 +55,33 @@ $user=$User->find(['acc'=>$_SESSION['login']]);
     <div class="all tt ct">總價:<?=$sum;?></div>
     <input type="hidden" name="total" value="<?=$sum;?>">
     <div class="ct">
-        <input type="button" value="確定送出" onclick="subitForm()">
+        <input type="button" value="確定送出" onclick="submitForm()">
         <input type="button" value="返回修改訂單" onclick="location.href='?do=buycart'">
     </div>
 </form>
 
 <script>
-    function subitForm(){
+    function submitForm(){
         alert("訂購成功\n感謝你的選購");
         $("#orderForm").submit();
     }
+
+    // $("#orderForm").on("submit",function (e) { 
+    //     e.preventDefault();
+    //     alert("訂購成功\n感謝你的選購");
+    //     $("#orderForm").submit();
+    //  })
+
+    // 原生JS
+    //  $("#orderForm").on("submit",function (e) { 
+    //     e.preventDefault();
+    //     let form = new FormData(document.getElementById("orderForm"));
+    //     let data = Object.fromEntries(form.entries());
+    //     $.post('./api/save_order.php',data,()=>{
+    //         alert("訂購成功\n感謝你的選購");
+    //         location.href='?';
+    //     })
+        
+    //  })
+
 </script>
